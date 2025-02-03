07a3ae04cc0149304c9f5c68c096ef65d579ac32
fabiandresss <fabianarmijo610@gmail.com>
2025-02-03 16:23:31 -0300

    agregamos login

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const router = express.Router();
 const User = require("../models/User");
 
+// POST: Registrar
 router.post("/register", async (req, res) => {
     try {
         const { username, email, password} = req.body;
@@ -15,6 +18,32 @@ router.post("/register", async (req, res) => {
         await newUser.save();
 
         res.status(201).json({ message: "Usuario registrado con exito", newUser });
+    } catch (error) {
+        res.status(500).json({message: "Error en el servidor", error});
+    }
+});
+
+router.post("/login", async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ message: "El usuario no existe" });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({ message: "La contraseña incorrecta" });
+        }
+
+        const token = jwt.sign(
+            { id: user._id},
+            process.env.JWT_SECRET || "secreto", 
+            { expiresIn: "1h" }
+        );
+
+        res.json ({ token, user });
     } catch (error) {
         res.status(500).json({message: "Error en el servidor", error});
     }
